aa4975b30c9b9312f5756af0c464f7af132fc910
Md. Meharab Islam <mdnibir178@gmail.com>
2022-03-17 22:16:03 +0600

    Scrollable

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,11 +8,219 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List catagoryItem = [
+    'Games',
+    'Foods',
+    'Mobiles',
+    'Groceries',
+    'Electronic',
+    'Computers',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        
+      body: Padding(
+        padding: EdgeInsets.only(top: 60, left: 20),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcom Meharab,',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                "Lets's do somethings!",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 220,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                '40% off during\nCovid-19',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: Image.asset(
+                                  'images/a.png',
+                                  height: 70,
+                                  width: 70,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 255, 94, 0),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 220,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                '40% off during\nCovid-19',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: Image.asset(
+                                  'images/a.png',
+                                  height: 70,
+                                  width: 70,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 65, 145, 236),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 220,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                '40% off during\nCovid-19',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: Image.asset(
+                                  'images/a.png',
+                                  height: 70,
+                                  width: 70,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 133, 71, 248),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Top Categories',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: Text(
+                      'View All',
+                      style: TextStyle(color: Colors.orange),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 40,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: catagoryItem.length,
+                  itemBuilder: (context, index) => Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Container(
+                      child: Padding(
+                        padding: EdgeInsets.all(12),
+                        child: Text(
+                          catagoryItem[index],
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(30)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 500,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemCount: 9,
+                  itemBuilder: (_, index) => Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
